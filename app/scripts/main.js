c52431f4b2b7e127e934b2b06972cd294e890034
Jeschke <Mark.Jeschke@sf-ltmac17157.corp.epsilon.com>
2016-01-25 10:45:59 -0800

    Added supported devices to the main.js.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,30 +3,34 @@
 	function htmlOutput(html) {
 		// When this function is called, close the unordered list.
 		html += '</ul>';
-		// Display the JSON content.
+		// Display the JSON content in the HTML within the 'content' ID div.
 		$('#content').html(html);
 	}
 	$(function(){
+		// When the submit button is tapped or clicked, grab the JSON feed from the iTunes API.
 		$('#productform').on('submit', function(event){
 			event.preventDefault();
 			$('#content').html('<center>Loading...</center>');
-			var entity = 'software'; // Options can be 'music', 'books', 'movies', or 'software'
+			var entity = 'software'; // Options can be 'music', 'ebooks', 'movies', or 'software'
 			var searchterm = $('#search').val(); // For field for input.
 			var requrl = 'https://itunes.apple.com/search?';
 			var affiliatetoken = '&at=10l3KX&ct=search';
 			var fullurl = requrl + '&entity=' + entity + '&term=' + searchterm;
 			$.getJSON(fullurl + '&callback=?', function(data) {
+				// Store paths and array lengths in their own variable.
 				var html = '<ul class="tracks">\n';
 				var resultCount = data.resultCount; // Grab the result count
 				var productName;
 				var authorName;
 				var itunesProductUrl;
-				var iconSize512;
 				var productDescription;
 				var productPrice;
 				var productVersion;
 				var sellerUrl;
+				var iconSize512;
 				var ipadScreenshotUrls;
+				var supportedDevices;
+				var supportedDevicesCount;
 				var screenshotUrls;
 				var screenshotUrlsCount;
 				var ipadScreenshotUrlsCount;
@@ -52,20 +56,34 @@
 					}
 					html += '<h3>Description</h3>';
 					html += '<p>' + productDescription + '</p>';
+					// If the app contains iPhone screenshots, display them.
 					screenshotUrls = data.results[n].screenshotUrls;
 					screenshotUrlsCount = screenshotUrls.length;
+					// Cycle through the array of images.
 					for (var i = 0; i < screenshotUrlsCount; i++) {
-						console.log('screenshotUrls[' + i + ']: ' + screenshotUrls[i]);
+						//console.log('screenshotUrls[' + i + ']: ' + screenshotUrls[i]);
 						html += '<img class="screenshots" src="' + screenshotUrls[i] + '" width="320" height="auto" alt="Screenshot ' + [i] + ' of ' + productName + '" />';
 					}
+					// If the app contains iPad screenshot, display them.
 					if (ipadScreenshotUrls != null) {
 						ipadScreenshotUrls = data.results[n].ipadScreenshotUrls;
 						ipadScreenshotUrlsCount = ipadScreenshotUrls.length;
+						// Cycle through the array of images.
 						for (var s = 0; s < ipadScreenshotUrlsCount; s++){
-							console.log('ipadScreenshotUrls[' + s + ']: ' + ipadScreenshotUrls[s]);
+							//console.log('ipadScreenshotUrls[' + s + ']: ' + ipadScreenshotUrls[s]);
 							html += '<img class="screenshots" src="' + ipadScreenshotUrls[s] + '" width="320" height="auto" alt="Screenshot ' + [s] + ' of ' + productName + '" />';
 						}
 					}
+					// List Supported iOS devices.
+					supportedDevices = data.results[n].supportedDevices;
+					supportedDevicesCount = supportedDevices.length;
+					html += '<h4>Support Devices</h4>';
+					html += '<ul>';
+					for (var d = 0; d < supportedDevicesCount; d++) {
+						console.log('supportedDevices[' + d + ']: ' + supportedDevices[d]);
+						html += supportedDevices[d] + '<br />';
+					}
+					html += '</ul>'; // Close unordered list.
 					html += '</li><hr />';
 				}
 				htmlOutput(html); // Inject the html output into the htmlOutput function.
